e827b6c1bcd704bb11e3172a0d48d7fbab4ca234
Levente Sermaul <levente.sermaul@ericsson.com>
2023-11-07 15:11:29 +0100

    review 2 part 2

--- weather.py
+++ weather.py
@@ -1,14 +1,26 @@
 import time
+from threading import Thread
 from WeatherGetter import WeatherGetter
 
 
 def main():
-    wg = WeatherGetter()
-    wg.get_weather()
+    
+    
+    city_list = ('Sopron', 'Pécs', 'Budapest', 'Kecskemét', 'Debrecen')
+    thread_list = []
+    
+    for city in city_list:
+        #thread = Thread(target=wg, args=(city,))
+        thread = WeatherGetter(city)
+        thread_list.append(thread)
+    for thread in thread_list:
+        thread.start()
+    for thread in thread_list:
+        thread.join()
 
 if __name__ == '__main__':
     start = time.time()
     main()
     end = time.time()
     total = end-start
-    print(f'Total time {total:.8f} seconds')
+    print(f'Total time {total:.8f} seconds')    
