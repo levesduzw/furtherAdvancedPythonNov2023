214a73dd6f080e7fe2afce8ee267745385f03f54
Levente Sermaul <levente.sermaul@ericsson.com>
2023-11-08 15:14:14 +0100

    Before "review 3" task

---
+++ review_3/weather/WeatherGetter.py
@@ -0,0 +1,64 @@
+from threading import Thread
+from datetime import datetime
+import requests
+import csv
+
+class WeatherGetter(Thread):
+    URL_TEMPLATE = 'http://api.openweathermap.org/data/2.5/weather?q={city_name}&units=metric&APPID={api_key}'
+    API_KEY = '957d663a2296945e39a56609740a2548'
+    
+    semaphore = None    # optional
+    
+    def __init__(self, city, semaphore, output_filename, output_file_rlock):
+        Thread.__init__(self)
+        self.city = city
+        self.output_filename = output_filename
+        self.output_file_rlock = output_file_rlock
+        self.semaphore = semaphore
+        
+    def query_website(self, query_url) -> list:
+        """Queries website
+
+        Returns:
+            list of objects: (timestamp, city, temp, wind_speed, wind_dir)
+        """        
+        self.semaphore.acquire()
+        response = requests.get(query_url)
+        self.semaphore.release()
+        data = response.json()
+        self.temperature = data['main']['temp']  # access the temperature property from the JSON
+        
+        dt = datetime.fromtimestamp(data['dt'])
+        
+        
+        return [datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                dt,
+                self.city,
+                data['main']['temp'],
+                data['wind']['speed'],
+                data['wind']['deg'],
+                ]        
+
+    def serialize(self, city_data):
+        # timestamp	city	temp	wind_speed	wind_dir
+        #city_dt = city_data[0].strftime('%Y-%m-%dT%H:%M:%SZ')
+        city_dt = city_data[1].strftime('%Y-%m-%d %H:%M:%S')
+        city_name = city_data[2]
+        print(f'{city_dt} Writing {city_name} data to file...')
+        with self.output_file_rlock:
+            with open(self.output_filename, 'a') as csvfile:
+                csv_writer = csv.writer(csvfile, delimiter=';', quoting=csv.QUOTE_MINIMAL)
+                csv_writer.writerow(city_data)
+    
+        
+    def run(self):
+        print(f'(Timestamp) Starting thread for city "{self.city}"')
+        query_url = self.URL_TEMPLATE.format(city_name=self.city, api_key=self.API_KEY)
+        
+        city_data = self.query_website(query_url)
+        self.serialize(city_data)
+        
+        print(f'(Timestamp) Got temperature {self.temperature} for city "{self.city}"')
+        
+        return None
+    
---
+++ review_3/weather/weather.py
@@ -0,0 +1,68 @@
+import time
+import pandas as pd
+import csv
+from threading import Semaphore, RLock
+from WeatherGetter import WeatherGetter
+from pathlib import Path
+
+semaphore_thread_count = 15
+output_filename_HU = 'weather_data_HU.csv'
+output_filename_IE = 'weather_data_IE.csv'
+
+
+def convert_to_xlsx(input_filename, output_filename):
+    df = pd.read_csv(input_filename, sep=';', on_bad_lines='warn', engine='python')
+    df.to_excel(output_filename, sheet_name="Data", index=False)
+
+
+def main():
+    city_list_HU = ('Sopron', 'Pécs', 'Budapest', 'Kecskemét', 'Debrecen')
+    city_list_IE = ["Dublin","Cork","Limerick","Galway","Waterford","Drogheda","Kilkenny","Wexford","Sligo","Clonmel","Dundalk","Bray","Ennis","Tralee","Carlow","Naas","Athlone","Letterkenny","Tullamore","Killarney","Arklow","Cobh","Castlebar","Midleton","Mallow","Ballina","Enniscorthy","Wicklow","Cavan","Athenry","Longford","Dungarvan","Nenagh","Trim","Thurles","Youghal","Monaghan","Buncrana","Ballinasloe","Fermoy","Westport","Carrick-on-Suir","Birr","Tipperary","Carrickmacross","Kinsale","Listowel","Clonakilty","Cashel","Macroom","Castleblayney","Kilrush","Skibbereen","Bundoran","Templemore","Clones","Newbridge","Mullingar","Balbriggan","Greystones","Leixlip","Tramore","Shannon","Gorey","Tuam","Edenderry","Bandon","Loughrea","Ardee","Mountmellick","Bantry","Boyle","Ballyshannon","Cootehill","Ballybay","Belturbet","Lismore","Kilkee","Granard"]
+    thread_list_HU = []
+    thread_list_IE = []
+    semaphore_HU = Semaphore(semaphore_thread_count)
+    semaphore_IE = Semaphore(semaphore_thread_count)
+    output_file_rlock_HU = RLock()
+    output_file_rlock_IE = RLock()
+    
+    if not Path('weather_data_HU.csv').is_file():
+        with open(output_filename_HU, 'w') as csvfile:
+            csv_writer = csv.writer(csvfile, delimiter=';', quoting=csv.QUOTE_MINIMAL)
+            csv_writer.writerow(['local timestamp', 'server timestamp', 'city', 'temp', 'wind_speed', 'wind_dir'])
+
+    if not Path('weather_data_IE.csv').is_file():
+        with open(output_filename_IE, 'w') as csvfile:
+            csv_writer = csv.writer(csvfile, delimiter=';', quoting=csv.QUOTE_MINIMAL)
+            csv_writer.writerow(['local timestamp', 'server timestamp', 'city', 'temp', 'wind_speed', 'wind_dir'])
+    
+    for city in city_list_HU:
+        thread = WeatherGetter(city, semaphore_HU, output_filename_HU, output_file_rlock_HU)
+        thread_list_HU.append(thread)
+    for thread in thread_list_HU:
+        thread.start()
+    for thread in thread_list_HU:
+        thread.join()
+        
+    print()
+    
+    for city in city_list_IE:
+        thread = WeatherGetter(city, semaphore_IE, output_filename_IE, output_file_rlock_IE)
+        thread_list_IE.append(thread)
+    for thread in thread_list_IE:
+        thread.start()
+    for thread in thread_list_IE:
+        thread.join()
+
+
+if __name__ == '__main__':
+    start = time.time()
+    main()
+    end = time.time()
+    total = end-start
+    print(f'Total query time {total:.8f} seconds')
+    start = time.time()
+    convert_to_xlsx(output_filename_HU, output_filename_HU.replace('.csv', '.xlsx'))
+    convert_to_xlsx(output_filename_IE, output_filename_IE.replace('.csv', '.xlsx'))
+    end = time.time()
+    total = end-start
+    print(f'Total file write time {total:.8f} seconds')
